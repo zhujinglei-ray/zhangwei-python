471593be0d85039f78d1e14042d3d7d34d96c5a9
zhujinglei <zhujinglei2020@outlook.com>
2022-04-18 18:45:20 +0100

    Rename to src

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from util.DataInterpretationUtils import DataDescriptionService
+from src.DataInterpretationUtils import DataDescriptionService
 
 # 程序 入口
 # 读取清理好的数据
 # data = Data()
 # data.get_cleaned_data()
 
+# 数据描述
 descriptionService = DataDescriptionService()
 
 descriptionService.describe_binary_value()
 descriptionService.describe_continuous_value()
 descriptionService.describe_categorical_variable()
 descriptionService.show_information_value()
+
+# 算法
--- src/DataInterpretationUtils.py
+++ src/DataInterpretationUtils.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-from util.Data import Data
+from src.Data import Data
 
 
 # 准备 information value 的表
